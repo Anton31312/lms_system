dfacc27d95768c422d7e8b6af5fa7b0f44f7775d
Антон Федяев <fearthe54@gmail.com>
2024-06-11 10:41:34 +0300

    cp.py fix

--- users/management/commands/cp.py
+++ users/management/commands/cp.py
@@ -1,14 +1,29 @@
 from django.core.management import BaseCommand
 
-from study.models import Lesson
+from study.models import Course
 from users.models import Payment, User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        user = User.objects.create(
+            email='test@sky.pro',
+            first_name='Test',
+            last_name='SkyPro',
+            is_staff=True,
+            is_active=True
+        )
+        user.set_password('123qwe456rty')
+        user.save()
+
+        course = Course.objects.create(
+            title='Изучение Go с нуля'
+        )
+        course.save()
+
         payment = Payment.objects.create(
             user=User.objects.get(pk=1),
-            lesson=Lesson.objects.get(pk=1),
+            course=Course.objects.get(pk=1),
             payment_amount=1000
         )
         payment.save()
